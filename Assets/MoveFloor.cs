6323e3b594cdb659f375f8be90942cb06bb0467d
kkzicong <kkzicong@gmail.com>
2016-03-06 16:59:29 -0500

    added moving blocks

---
+++ Assets/MoveFloor.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveFloor : MonoBehaviour
+{
+    public float speed;
+
+    bool movePos;
+    Vector3 dir;
+    float timer;
+
+    void Start()
+    {
+        timer = 0f;
+
+        if (gameObject.name == "Moving Floor")
+        {
+            dir = Vector3.forward;
+
+        } else
+        {
+            dir = Vector3.up;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= 10f)
+        {
+            dir = -dir;
+            timer = 0f;
+        }
+
+        transform.Translate(-dir * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.parent = transform;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+        }
+    }
+}
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,41 +1,4 @@
-﻿//using UnityEngine;
-//using System.Collections;
-
-//public class CameraController : MonoBehaviour
-//{
-
-//    public GameObject PlayerChar;
-//    public float cameraXval = 0;
-//    public float cameraYval = 3;
-//    public float cameraZval = -5;
-
-//    // Use this for initialization
-//    void Start()
-//    {
-
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-
-//        Vector3 cameraPosit = transform.position;
-
-//        float cameraX = ((PlayerChar.transform.position.x + cameraXval) * .05F) + (transform.position.x * .95F);
-//        float cameraY = ((PlayerChar.transform.position.y + cameraYval) * .05F) + (transform.position.y * .95F);
-//        float cameraZ = ((PlayerChar.transform.position.z + cameraZval) * .05F) + (transform.position.z * .95F);
-
-//        transform.position = new Vector3(cameraX, cameraY, cameraZ);
-//        cameraPosit = new Vector3(cameraX - cameraXval, cameraY - cameraYval, cameraZ - cameraZval);
-//        transform.LookAt(cameraPosit);
-
-//        //}
-
-//    }
-
-//}
-
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 [AddComponentMenu("Camera-Control/Mouse Orbit with zoom")]
@@ -81,7 +44,7 @@ public class CameraController : MonoBehaviour
             x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
             y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
 
-            y = ClampAngle(y, yMinLimit, yMaxLimit);
+            y = Mathf.Clamp(y, yMinLimit, yMaxLimit);
 
             Quaternion rotation = Quaternion.Euler(y, x, 0);
 
@@ -98,14 +61,5 @@ public class CameraController : MonoBehaviour
             transform.rotation = rotation;
             transform.position = position;
         }
-    }
-
-    public static float ClampAngle(float angle, float min, float max)
-    {
-        if (angle < -360F)
-            angle += 360F;
-        if (angle > 360F)
-            angle -= 360F;
-        return Mathf.Clamp(angle, min, max);
     }
 }
