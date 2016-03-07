ec6bfc58f27016ffdba8b43dc8a4c32355527a22
kkzicong <kkzicong@gmail.com>
2016-03-07 01:12:15 -0500

    added moving walls

--- Assets/Scripts/WallScale.cs
+++ Assets/Scripts/WallScale.cs
@@ -2,13 +2,27 @@
 using System.Collections;
 
 public class WallScale : MonoBehaviour {
+    public float scale;
     public float scaleFactor;
+    public float interval;
 
     float timer;
 
+    void Start()
+    {
+        timer = 0f;
+    }
+
     // Update is called once per frame
     void Update () {
-     //   if (timer >= )
-	    //transform.localScale 
-	}
+        if (timer >= interval)
+        {
+            scaleFactor = -scaleFactor;
+            timer = 0f;
+        }
+
+        scale += scaleFactor;
+        transform.localScale = new Vector3(1, 1, scale);
+        timer += Time.deltaTime;
+    }
 }
